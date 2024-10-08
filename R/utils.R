937875d2778896d1898a4d23df6a5e36b7c2b311
Shubham Dutta <shubhamdutta26@protonmail.com>
2024-10-08 14:51:44 -0400

    new R file utils

---
+++ R/utils.R
@@ -0,0 +1,10 @@
+# Define the number of rows and columns for each plate type
+plate_dims <- list(
+  `6` = c(2, 3),
+  `12` = c(3, 4),
+  `24` = c(4, 6),
+  `48` = c(6, 8),
+  `96` = c(8, 12),
+  `384` = c(16, 24),
+  `1536` = c(32, 48)
+)
