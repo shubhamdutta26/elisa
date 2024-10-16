739369c75eee9eb9bef6d49d7f096f5919d3af1f
Shubham Dutta <shubhamdutta26@protonmail.com>
2024-10-15 20:53:13 -0400

    added .data

--- R/globals.R
+++ R/globals.R
@@ -1,2 +1,2 @@
 # Declare global variables to avoid "no visible binding for global variable" note
-utils::globalVariables(c("od450", "mean_blank", "blanked_od", "primary_mab", "mean_od", "mean_sd"))
+utils::globalVariables(c("od450", "mean_blank", "blanked_od", "primary_mab", "mean_od", "mean_sd", ".data"))
